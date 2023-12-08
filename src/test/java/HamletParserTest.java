f440e4e0a26004bbf60829ed739c014527e9bd85
hyunjialee <hyjialee@gmail.com>
2023-12-07 22:59:33 -0500

    setting test cases

--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,3 +1,4 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -15,17 +16,40 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String toChange = "The Tragedy of Hamlet, Prince of Denmark";
+        String expected = "The Tragedy of Leon, Prince of Denmark";
+
+        String actual = "";
+
+        assertEquals(expected, actual);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String toChange = "What, is Horatio there?";
+        String expected = "What, is Tariq there?";
+
+        String actual = "";
+
+        assertEquals(expected,actual);
+
     }
 
     @Test
     public void testFindHoratio() {
+
+        String toFind = "Well, good night.\n" +
+                "If you do meet Horatio and Marcellus,\n" +
+                "The rivals of my watch, bid them make haste.";
+        ;
+
     }
 
     @Test
     public void testFindHamlet() {
+
+        String toFind = "Good Hamlet, cast thy nighted colour off,\n" +
+                "And let thine eye look like a friend on Denmark.\n" +
+                "Do not for ever with thy vailed lids";
     }
 }
